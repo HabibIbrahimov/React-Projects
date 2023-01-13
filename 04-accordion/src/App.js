6165b3e249ec1968da2eb8cedceda9b0e1418a85
HabibIbrahimov <habibyi@code.edu.az>
2023-01-13 19:11:34 +0400

    Added Menu Items

--- 04-accordion/src/App.js
+++ 04-accordion/src/App.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 import data from './data';
 import SingleQuestion from './Question';
 function App() {
-  const [questions, setQuestions] = useState(data);
+  const [questions] = useState(data);
   return (
     <main>
-      <div className='container'>
+      <div className="container">
         <h3>questions and answers about login</h3>
-        <section className='info'>
+        <section className="info">
           {questions.map((question) => {
             return (
               <SingleQuestion key={question.id} {...question}></SingleQuestion>
