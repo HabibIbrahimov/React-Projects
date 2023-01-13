97fed0a53a658e6cba5928b13eb9e1f71e7ca7b8
HabibIbrahimov <habibyi@code.edu.az>
2023-01-13 14:01:34 +0400

    Remove Tour

--- 02-tours/src/App.js
+++ 02-tours/src/App.js
@@ -8,6 +8,10 @@ function App() {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
 
+  const removeTour = (id) => {
+    const newTour = tours.filter((tour) => tour.id !== id);
+    setTours(newTour);
+  };
   const fetchTours = async () => {
     setLoading(true);
     try {
@@ -30,9 +34,21 @@ function App() {
       </main>
     );
   }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={fetchTours}>
+            Refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
   return (
     <main>
-      <Tours tours={tours} />
+      <Tours tours={tours} removeTour={removeTour} />
     </main>
   );
 }
--- 02-tours/src/Tour.js
+++ 02-tours/src/Tour.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Tour = ({ id, image, info, name, price }) => {
+const Tour = ({ id, image, info, name, price, removeTour }) => {
   const [readMore, setReadMore] = useState(false);
   return (
     <article className="single-tour">
@@ -16,7 +16,9 @@ const Tour = ({ id, image, info, name, price }) => {
             {readMore ? 'show less' : 'read more'}
           </button>
         </p>
-        <button className="delete-btn">not interested</button>
+        <button className="delete-btn" onClick={() => removeTour(id)}>
+          not interested
+        </button>
       </footer>
     </article>
   );
