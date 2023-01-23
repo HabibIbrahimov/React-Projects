90a08de4ad8ac6796a53ff6fb0d6f2cfd672a1d6
HabibIbrahimov <habibyi@code.edu.az>
2023-01-23 22:31:04 +0400

    Page Components

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,5 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+
 function App() {
-  return <h2>React Router 6 Tutorial</h2>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
