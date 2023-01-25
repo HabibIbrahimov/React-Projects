eed32d3baa6bbdb959f82ef9c3e42e416b2fbd7c
HabibIbrahimov <habibyi@code.edu.az>
2023-01-25 13:47:39 +0400

    About and Error Page

--- 09-cocktails/src/pages/About.js
+++ 09-cocktails/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+        voluptates, quod, quia, voluptatibus quae voluptatem quidem quos
+        accusantium nemo voluptatum quibusdam. Quisquam, quae. Quisquam
+        voluptates, quod, quia, voluptatibus quae voluptatem quidem quos
+        accusantium nemo voluptatum quibusdam. Quisquam, quae.
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
