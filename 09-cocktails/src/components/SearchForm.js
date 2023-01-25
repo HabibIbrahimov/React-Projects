86b064427252ab26b1e18a73158857237211fc27
HabibIbrahimov <habibyi@code.edu.az>
2023-01-25 19:31:17 +0400

    Search Form

--- 09-cocktails/src/components/SearchForm.js
+++ 09-cocktails/src/components/SearchForm.js
@@ -3,10 +3,33 @@ import { useGlobalContext } from '../context';
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef('');
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h2>search form component</h2>
-    </div>
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
   );
 };
 
