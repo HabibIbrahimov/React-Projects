9df7630ccdba3b1c89984925c4403f2f4e5d65b4
HabibIbrahimov <habibyi@code.edu.az>
2023-01-25 15:20:47 +0400

    Context Api Setup

--- 09-cocktails/src/components/CocktailList.js
+++ 09-cocktails/src/components/CocktailList.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import Cocktail from './Cocktail'
-import Loading from './Loading'
-import { useGlobalContext } from '../context'
+import React from 'react';
+import Cocktail from './Cocktail';
+import Loading from './Loading';
+import { useGlobalContext } from '../context';
 
 const CocktailList = () => {
+  const { cocktails, loading } = useGlobalContext();
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <div>
       <h2>cocktail list component</h2>
     </div>
-  )
-}
+  );
+};
 
-export default CocktailList
+export default CocktailList;
