e10f1f12fc4842537a0a1a8fa1e8bcb06c71332d
HabibIbrahimov <habibyi@code.edu.az>
2023-01-25 17:02:58 +0400

    Display List

--- 09-cocktails/src/components/CocktailList.js
+++ 09-cocktails/src/components/CocktailList.js
@@ -9,9 +9,14 @@ const CocktailList = () => {
     return <Loading />;
   }
   return (
-    <div>
-      <h2>cocktail list component</h2>
-    </div>
+    <section className="section">
+      <h2 className="section-title">cocktails</h2>
+      <div className="cocktails-center">
+        {cocktails.map((item) => {
+          return <Cocktail key={item.id} {...item} />;
+        })}
+      </div>
+    </section>
   );
 };
 
